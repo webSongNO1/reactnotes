027f4dadf6f806cbfc910faa54ea7449e2d03eb9
songyu <289483936@qq.com>
2024-07-06 17:52:37 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useHookstate } from "@hookstate/core";
+let obj = {
+	name: "",
+	age: ""
+}
+function App() {
+	// const [count, setCount] = useState(0);
+	const count = useHookstate(0);
+	return (
+		<>
+			<div>
+				<a href='https://vitejs.dev' target='_blank'>
+					<img src={viteLogo} className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://react.dev' target='_blank'>
+					<img src={reactLogo} className='logo react' alt='React logo' />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<button onClick={() => count.set((n) => n + 1)}>
+					count is {count.get()}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
+}
+
+export default App;
